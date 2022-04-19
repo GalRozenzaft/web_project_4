646824256a58df8a3a15ffdcf3979b3e1021c748
Gal Rozenzaft <galrozenzaft@gmail.com>
2022-04-19 13:39:21 +0300

    web project 4

---
+++ script/index.js
@@ -0,0 +1,31 @@
+let profile = document.querySelector(".page");
+let modal = document.querySelector(".modal");
+let editButton = document.querySelector(".profile__edit-profile-button");
+let closeButton = document.querySelector(".modal-container__close-button");
+let submitButton = document.querySelector(".modal-container__submit-button");
+let profileName = document.querySelector(".profile__name");
+let aboutMe = document.querySelector(".profile__about-me");
+let nameInput = document.querySelector("#name");
+let aboutMeInput = document.querySelector("#about-me");
+let formElement = document.querySelector(".form");
+
+function handleEditButton() {
+  nameInput.value = profileName.textContent;
+  aboutMeInput.value = aboutMe.textContent;
+  modal.classList.remove("modal_invisible");
+}
+
+function handleCloseButton() {
+  modal.classList.add("modal_invisible");
+}
+
+function handleProfileFormSubmit(event) {
+  event.preventDefault();
+  profileName.textContent = nameInput.value;
+  aboutMe.textContent = aboutMeInput.value;
+  modal.classList.add("modal_invisible");
+}
+
+editButton.addEventListener("click", handleEditButton);
+closeButton.addEventListener("click", handleCloseButton);
+formElement.addEventListener("submit", handleProfileFormSubmit);
