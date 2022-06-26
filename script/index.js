8e8932a1769f204a4c3a422d92bdd22205be823a
galrozenzaft <galrozenzaft@gmail.com>
2022-06-26 19:06:55 +0300

    add project 6

--- script/index.js
+++ script/index.js
@@ -151,6 +151,38 @@ function openZoomedPlaceModal(place) {
   toggleModal(zoomedPlaceModal);
 }
 
+// Validating the "Edit Profile" Form
+
+const formElement = document.querySelector(".form");
+const formInput = formElement.querySelector(".form__input");
+const formError = formElement.querySelector(`.${formInput.id}-error`);
+
+const showInputError = (element) => {
+  console.log("showInputError was called");
+  formInput.classList.add("form__input_type_error");
+  formError.classList.add("form__input-error-message_active");
+};
+
+const hideInputError = (element) => {
+  console.log("hideInputError was called");
+  formInput.classList.remove("form__input_type_error");
+  formError.classList.remove("form__input-error-message_active");
+};
+
+const isValid = () => {
+  if (!formInput.validity.valid) {
+    showInputError(formInput);
+  } else {
+    hideInputError(formInput);
+  }
+};
+
+formElement.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+});
+
+formInput.addEventListener("input", isValid);
+
 // Like And Delete Buttons Handlers
 
 function handleLikeButtonToggle(event) {
